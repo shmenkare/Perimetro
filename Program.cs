89bbac6f42e29d1cade773a53010a2044fe4473b
“shmenkare” <shmenkare@gmail.com>
2023-08-27 18:34:24 -0400

    Perimetro 2.0

--- Program.cs
+++ Program.cs
@@ -8,19 +8,48 @@ namespace Perimetro
     {
         static void Main(string[] args)
         {
+            //Perimetro 2.0
+            /* Area= (a*p)/2
+             perimetro= numero de lados * long lados */
+
             int numLados = 0;
+            float apotema = 1.0f;
             float lonLados = 1.0f;
             float perimetro = 1.0f;
+            float area = 1.0f;
+            int opcion = 0;
 
-            Console.WriteLine("Dame el numero de lados del poligono");
-            numLados = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Dame la longitud de los lados del poligono");
-            lonLados = Convert.ToSingle(Console.ReadLine());
+            Console.WriteLine("Que desea calcular");
+            Console.WriteLine("1- Perimetro");
+            Console.WriteLine("2- Area");
+            opcion = Convert.ToInt32(Console.ReadLine());
 
-            perimetro = numLados * lonLados;
+            if (opcion == 1)
+            {
+                Console.WriteLine("Dame el numero de lados del poligono");
+                numLados = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("El perimetro del poligono es {0}", perimetro);
+                Console.WriteLine("Dame la longitud de los lados del poligono");
+                lonLados = Convert.ToSingle(Console.ReadLine());
+
+                perimetro = numLados * lonLados;
+
+                Console.WriteLine("El perimetro del poligono es {0}", perimetro);
+            
+            }
+            else if (opcion == 2)
+            {
+                Console.WriteLine("Dame el valor de la apotema");
+                apotema = Convert.ToSingle(Console.ReadLine());
+
+                Console.WriteLine("Dame el valor del perimetro");
+                perimetro = Convert.ToSingle(Console.ReadLine());
+
+                area = (apotema * perimetro)/2;
+
+                Console.WriteLine("El Area del poligono es {0}", area);
+            }
 
             Console.ReadKey();
 
