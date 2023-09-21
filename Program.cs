e03784bf7cafb642382a2b8e9ea9dc2175c522ec
“shmenkare” <shmenkare@gmail.com>
2023-09-20 22:40:29 -0400

    v3.0

--- Program.cs
+++ Program.cs
@@ -8,15 +8,16 @@ namespace Perimetro
     {
         static void Main(string[] args)
         {
-            //Perimetro 2.0
+            //Perimetro 3.0
             /* Area= (a*p)/2
              perimetro= numero de lados * long lados */
 
             int numLados = 0;
             float apotema = 1.0f;
             float lonLados = 1.0f;
-            float perimetro = 1.0f;
+            float resul = 1.0f;
             float area = 1.0f;
+            float perim = 1.0f;
             int opcion = 0;
 
 
@@ -27,15 +28,16 @@ namespace Perimetro
 
             if (opcion == 1)
             {
+               
                 Console.WriteLine("Dame el numero de lados del poligono");
                 numLados = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Dame la longitud de los lados del poligono");
                 lonLados = Convert.ToSingle(Console.ReadLine());
 
-                perimetro = numLados * lonLados;
-
-                Console.WriteLine("El perimetro del poligono es {0}", perimetro);
+                resul = Perimetro(numLados, lonLados);
+                
+                Console.WriteLine("El perimetro del poligono es {0}", resul);
             
             }
             else if (opcion == 2)
@@ -44,16 +46,37 @@ namespace Perimetro
                 apotema = Convert.ToSingle(Console.ReadLine());
 
                 Console.WriteLine("Dame el valor del perimetro");
-                perimetro = Convert.ToSingle(Console.ReadLine());
+                perim = Convert.ToSingle(Console.ReadLine());
 
-                area = (apotema * perimetro)/2;
+                resul = Area(apotema, perim);
 
-                Console.WriteLine("El Area del poligono es {0}", area);
+                Console.WriteLine("El Area del poligono es {0}", resul);
+            }
+            else
+            {
+                Console.WriteLine("Opcion no valida");
             }
 
             Console.ReadKey();
 
 
+        }
+        static float Perimetro(int n1, float n2)
+        {
+            float r = 0.0f;
+
+            r = n1 * n2;
+
+            return r;
+        }
+
+        static float Area(float a1, float a2)
+        {
+            float r = 0.0f;
+
+            r = (a1 * a2)/2;
+
+            return r;
         }
     }
 }
